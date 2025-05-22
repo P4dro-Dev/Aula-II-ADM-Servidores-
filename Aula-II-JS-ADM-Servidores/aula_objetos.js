bc78bf3d4f5aae2c35ce9f554d6169a26354afcd
Pedro Henrique <Ph9084533@gmail.com>
2025-05-22 17:58:44 +0000

    Adicionadando script modelo, para projetos futuros

---
+++ Aula-II-JS-ADM-Servidores/aula_objetos.js
@@ -0,0 +1,18 @@
+// Exemplos de como desenvolver objetos em JavaScript
+const pessoa = {
+
+    nome: "Ana",
+
+    falar: function () {
+        retur `Meu nome é ${this.nome}`;
+    },
+    andar: function () {
+        return `Estou andando`;
+    },
+    cantar: function (){
+        return `${this.nome} está cantando`;
+    }
+
+};
+
+// 
